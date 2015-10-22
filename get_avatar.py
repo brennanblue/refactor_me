9f43698d4ea10c081fd74811ac683d824d544a46
brennanblue <brennanblue@gmail.com>
2015-10-21 18:19:45 -1000

    added whitespace

--- get_avatar.py
+++ get_avatar.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # COMMAND LINE PROGRAM TO DOWNLOAD A USER'S AVATAR FROM GITHUB. USAGE: `PYTHON get_avatar.py <GITHUB_USERNAME>`. 
+
 import sys, json
 import argparse
 import requests
@@ -10,18 +11,22 @@ import shutil
 parser = argparse.ArgumentParser()
 parser.add_argument(  'username')
 args = parser.parse_args()
+
 # CALL THE GITHUB API AND GET USER INFO
 RequestUrl = 'https://api.github.com/users/' + args.username
 
 result = requests.get( RequestUrl )
+
 if result.ok :
     user_info = json.loads(result.content)
-    avatarURL = user_info['avatar_url']
+    avatarUrl = user_info['avatar_url']
 else:
     sys.stderr.write( "Error fetching user information for {0}; exiting now, sorry...\n".format(args.username) )
     sys.exit()
+
 # DOWNLOAD AND SAVE IMAGE FILE
-i = requests.get(avatarURL , stream=True)
+i = requests.get(avatarUrl , stream=True)
+
 if i.ok:
     with open(args.username + '.png' , 'wb') as outfile:
         shutil.copyfileobj( i.raw,  outfile )
